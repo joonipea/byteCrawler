70bd38fbc877c1c22fd1ae533d20d0eaf1c013f5
June Pratt <juneprattm@gmail.com>
2024-03-01 11:00:20 -0500

    level up screen styling for mobile

--- src/screens/levelUpScreen.js
+++ src/screens/levelUpScreen.js
@@ -14,6 +14,7 @@ const LevelUpScreen = ({ setParent, learnedCards }) => {
         mp: context.character.stats.mp,
         maxMP: context.character.stats.maxMP,
     });
+    const [subScreen, setSubScreen] = useState(0);
 
     const increaseStat = (stat) => {
         if (statPoints > 0) {
@@ -58,90 +59,131 @@ const LevelUpScreen = ({ setParent, learnedCards }) => {
 
     return (
         <div className="level-up--container">
-            <p className="level-up--text">Level Up!</p>
-            <p className="level-up--text">
-                You are now level {context.character.level + 1}
-            </p>
-            <p className="level-up--text">
-                You have {statPoints} stat points to spend
-            </p>
-            <div className="stat-container">
-                <button
-                    className="btn"
-                    onClick={() => decreaseStat("maxHealth")}>
-                    -
-                </button>
-                <p>Max Health: {currentStats.maxHealth}</p>
-                <button
-                    className="btn"
-                    onClick={() => increaseStat("maxHealth")}>
-                    +
-                </button>
-            </div>
-            <div className="stat-container">
-                <button className="btn" onClick={() => decreaseStat("maxMP")}>
-                    -
-                </button>
-                <p>Max MP: {currentStats.maxMP}</p>
-                <button className="btn" onClick={() => increaseStat("maxMP")}>
-                    +
-                </button>
-            </div>
-            <div className="stat-container">
-                <button className="btn" onClick={() => decreaseStat("attack")}>
-                    -
-                </button>
-                <p>Attack: {currentStats.attack}</p>
-                <button className="btn" onClick={() => increaseStat("attack")}>
-                    +
-                </button>
-            </div>
-            <div className="stat-container">
-                <button className="btn" onClick={() => decreaseStat("defense")}>
-                    -
-                </button>
-                <p>Defense: {currentStats.defense}</p>
-                <button className="btn" onClick={() => increaseStat("defense")}>
-                    +
-                </button>
-            </div>
-            <div className="stat-container">
-                <button className="btn" onClick={() => decreaseStat("luck")}>
-                    -
-                </button>
-                <p>Luck: {currentStats.luck}</p>
-                <button className="btn" onClick={() => increaseStat("luck")}>
-                    +
-                </button>
-            </div>
-            <div>
-                <p>You Learned:</p>
-                <div className="deck-container">
-                    {learnedCards.map((card, index) => {
-                        let newCard = card;
-                        const cardProfeciencies = newCard.stats;
-                        let damage = 0;
-                        const stats = currentStats;
-                        for (let profeciency of cardProfeciencies) {
-                            damage += Math.ceil(
-                                stats[profeciency[0]] * profeciency[1]
-                            );
-                        }
-                        newCard.damage = damage;
-                        return (
-                            <Card
-                                key={index}
-                                card={newCard}
-                                onClick={() => {}}
-                            />
-                        );
-                    })}
-                </div>
-            </div>
+            {subScreen === 0 && (
+                <>
+                    <p className="level-up--text">Level Up!</p>
+                    <p className="level-up--text">
+                        You are now level {context.character.level + 1}
+                    </p>
+                    <p className="level-up--text">
+                        You have {statPoints} stat points to spend
+                    </p>
+                    <div className="stat-container">
+                        <button
+                            className="btn"
+                            onClick={() => decreaseStat("maxHealth")}>
+                            -
+                        </button>
+                        <p>Max Health: {currentStats.maxHealth}</p>
+                        <button
+                            className="btn"
+                            onClick={() => increaseStat("maxHealth")}>
+                            +
+                        </button>
+                    </div>
+                    <div className="stat-container">
+                        <button
+                            className="btn"
+                            onClick={() => decreaseStat("maxMP")}>
+                            -
+                        </button>
+                        <p>Max MP: {currentStats.maxMP}</p>
+                        <button
+                            className="btn"
+                            onClick={() => increaseStat("maxMP")}>
+                            +
+                        </button>
+                    </div>
+                    <div className="stat-container">
+                        <button
+                            className="btn"
+                            onClick={() => decreaseStat("attack")}>
+                            -
+                        </button>
+                        <p>Attack: {currentStats.attack}</p>
+                        <button
+                            className="btn"
+                            onClick={() => increaseStat("attack")}>
+                            +
+                        </button>
+                    </div>
+                    <div className="stat-container">
+                        <button
+                            className="btn"
+                            onClick={() => decreaseStat("defense")}>
+                            -
+                        </button>
+                        <p>Defense: {currentStats.defense}</p>
+                        <button
+                            className="btn"
+                            onClick={() => increaseStat("defense")}>
+                            +
+                        </button>
+                    </div>
+                    <div className="stat-container">
+                        <button
+                            className="btn"
+                            onClick={() => decreaseStat("luck")}>
+                            -
+                        </button>
+                        <p>Luck: {currentStats.luck}</p>
+                        <button
+                            className="btn"
+                            onClick={() => increaseStat("luck")}>
+                            +
+                        </button>
+                    </div>
+                    {learnedCards.length > 0 ? (
+                        <button
+                            onClick={() => {
+                                if (statPoints > 0) return;
+                                setSubScreen(1);
+                            }}
+                            className="btn">
+                            View New Cards
+                        </button>
+                    ) : (
+                        <button onClick={() => confirm()} className="btn">
+                            Confirm
+                        </button>
+                    )}
+                </>
+            )}
+            {subScreen === 1 && (
+                <>
+                    <p>You Learned:</p>
 
-            <button onClick={() => confirm()} className="btn">
-                Confirm
-            </button>
+                    <div className="deck-container">
+                        {learnedCards.map((card, index) => {
+                            let newCard = card;
+                            const cardProfeciencies = newCard.stats;
+                            let damage = 0;
+                            const stats = currentStats;
+                            for (let profeciency of cardProfeciencies) {
+                                damage += Math.ceil(
+                                    stats[profeciency[0]] * profeciency[1]
+                                );
+                            }
+                            newCard.damage = damage;
+                            return (
+                                <Card
+                                    key={index}
+                                    card={newCard}
+                                    onClick={() => {}}
+                                />
+                            );
+                        })}
+                    </div>
+                    <div className="btn-container--horizontal">
+                        <button onClick={() => setSubScreen(0)} className="btn">
+                            Back
+                        </button>
+                        <button onClick={() => confirm()} className="btn">
+                            Close
+                        </button>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
